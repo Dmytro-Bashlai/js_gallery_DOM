2c3926e1de852ed445666fcffb38268e896f1cb6
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-20 22:50:02 +0300

    make build

---
+++ index.bacd3621.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector(".gallery__large-img");e&&t&&e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item").querySelector(".list-item__link").href;t.setAttribute("src",r)});
+//# sourceMappingURL=index.bacd3621.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const gallery = document.querySelector(".gallery__list");
+const largeImage = document.querySelector(".gallery__large-img");
+if (gallery && largeImage) gallery.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const linkHref = e.target.closest(".list-item").querySelector(".list-item__link").href;
+    largeImage.setAttribute("src", linkHref);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
